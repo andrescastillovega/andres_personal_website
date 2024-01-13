23267f4a6de915d0acece4bc53d57acbf1f059a9
andres <andcastillove@unal.edu.co>
2024-01-13 10:02:37 -0500

    fixed particles as the background

--- assets/particles/config.js
+++ assets/particles/config.js
@@ -36,7 +36,7 @@ particlesJS("particles-js", {
         }
       },
       "size": {
-        "value": 3,
+        "value": 4,
         "random": true,
         "anim": {
           "enable": false,
